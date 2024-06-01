ad308e73e04bfd5c48816fee03a1d158620fc485
Finnral <finnral1st@gmail.com>
2024-06-02 02:16:07 +0800

    add property

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin,LoginManager,login_required,login_user,logout_user
 
 app = Flask(__name__)
 
@@ -11,6 +12,22 @@ db = SQLAlchemy(app)
 class Book(db.Model):
     book_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+
+class Property(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(50), nullable=False, default='select')
+    images = db.relationship('PropertyImage', backref='property', lazy=True)
+    date_added = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+# Model PropertyImage (sesuaikan dengan kebutuhan Anda)
+class PropertyImage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
 
 # Commit your model (table) to the database
 with app.app_context():
@@ -30,6 +47,30 @@ def add_book():
         return redirect(url_for('home'))
     return render_template('add_book.html')
 
+@app.route('/add_property', methods=['GET', 'POST'])
+def add_property():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        price = request.form['price']
+        location = request.form['location']
+        type = request.form['type']
+        date_added = request.form['date_added']
+        
+        new_property = Property(
+            title=title,
+            description=description,
+            price=price,
+            location=location,
+            type=type,
+            date_added=date_added
+        )
+        
+        db.session.add(new_property)
+        db.session.commit()
+        
+        return redirect(url_for('home'))
+    return render_template('add_property.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
